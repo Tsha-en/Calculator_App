166d93d14f69932c9177465ebddbf34518287577
Антон <anton@MacBook-Pro.local>
2024-12-16 11:51:51 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Tsha-en/Middle-Final-Project
+
+go 1.23.3
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+	var i int
+	fmt.Scan(&i)
+	fmt.Print(i)
+}
